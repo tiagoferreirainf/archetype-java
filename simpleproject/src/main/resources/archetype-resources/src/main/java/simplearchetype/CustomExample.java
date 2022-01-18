b709b43a14b40811b7929b9060aae3e817b24f1c
Tiago Ferreira <tiago.ferreira.inf@gmail.com>
2022-01-18 22:14:18 +0000

    add example for a single module archetype project

---
+++ simpleproject/src/main/resources/archetype-resources/src/main/java/simplearchetype/CustomExample.java
@@ -0,0 +1,8 @@
+package ${groupId}.simplearchetype;
+
+
+public class CustomExample {
+    public void printName(String name) {
+        System.out.println("Hello" + name);
+    }
+}
---
+++ simpleproject/target/classes/archetype-resources/src/test/java/simplearchetype/CustomExampleTest.java
@@ -0,0 +1,9 @@
+package ${groupId}.simplearchetype;
+
+
+public class CustomExampleTest {
+    public static void main(String[] args) {
+        $.CustomExample example = new CustomExample();
+        example.printName("Test 123");
+    }
+}
