f65cb1194a16d2b7e5675528ef1b768aa737d7a4
Tiago Ferreira <tiago.ferreira.inf@gmail.com>
2022-01-20 21:51:20 +0000

    added springboot dockerimage and helmchart archetype

---
+++ springdockerhelm/src/main/resources/archetype-resources/__modulename__-microservice/src/main/java/SpringMainApplication.java
@@ -0,0 +1,11 @@
+package ${package};
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMainApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMainApplication.class, args);
+	}
+}
